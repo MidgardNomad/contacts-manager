406973459d93f725c97a91856c6accdc64645956
M-E-Taha <elsha3rawy.btm@gmail.com>
2025-06-08 02:26:42 +0300

    Finish auth service

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { CrudService } from './crud.service';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private crud: CrudService) {}
+
+  private readonly endpoint = 'auth/login';
+
+  login(data: User) {
+    return this.crud.post<{ token: string }, User>(this.endpoint, { ...data });
+  }
 }
