8317c9c5b70cb3473339666360571c04ce4ca54c
M-E-Taha <elsha3rawy.btm@gmail.com>
2025-06-07 04:58:53 +0300

    form validtion

--- client/src/app/modules/auth/auth.component.ts
+++ client/src/app/modules/auth/auth.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: false,
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit, OnDestroy {
+  constructor(private fb: FormBuilder) {}
+  //Life-Cycle Hooks
+  ngOnInit(): void {
+    this.initForm();
+  }
+  ngOnDestroy(): void {}
 
+  //DOM
+  @ViewChild('passwordInputField')
+  passwordInputField: ElementRef<HTMLInputElement>;
+  //Private Props
+
+  //Private methods
+  private initForm() {
+    this.signInForm = this.fb.group({
+      username: [null, Validators.required],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+    });
+  }
+  //Comp Props
+  signInForm: FormGroup;
+  //Comp Methods
+  togglePassword() {
+    this.passwordInputField.nativeElement.type =
+      this.passwordInputField.nativeElement.type === 'password'
+        ? 'text'
+        : 'password';
+  }
+  signin() {
+    console.log(this.signInForm.value);
+  }
 }
