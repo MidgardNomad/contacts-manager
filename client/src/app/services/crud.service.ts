5781dbf27686d923fa76f7eca1ebb72cd8b5be80
M-E-Taha <elsha3rawy.btm@gmail.com>
2025-06-08 02:22:28 +0300

    Finish the crud service.

--- client/src/app/services/crud.service.ts
+++ client/src/app/services/crud.service.ts
@@ -1,9 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { ServerResponse } from '../models/server-response.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CrudService {
   constructor(private http: HttpClient) {}
+
+  get<T>(endpoint: string) {
+    return this.http.get<T>(`${environment}/${endpoint}`);
+  }
+
+  post<T, D>(endpoint: string, data: D) {
+    return this.http.post<ServerResponse<T>>(
+      `${environment}/${endpoint}`,
+      data
+    );
+  }
+
+  put<T, D>(endpoint: string, data: D) {
+    return this.http.put<ServerResponse<T>>(`${environment}/${endpoint}`, data);
+  }
 }
