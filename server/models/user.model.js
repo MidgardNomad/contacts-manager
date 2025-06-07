bdac27a397ed2d6258144ac4bb1d146a34ee4376
M-E-Taha <elsha3rawy.btm@gmail.com>
2025-06-08 00:34:52 +0300

    Add static method to the user model to find the user by their username

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,16 +1,26 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const UserSchema = new schema({
-  username: {
-    type: String,
-    required: true,
+const UserSchema = new schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    collection: "users",
+    statics: {
+      findOnebyUserName(username) {
+        return this.findOne({ username });
+      },
+    },
+  }
+);
 
 module.exports.schema = UserSchema;
 module.exports.model = mongoose.model("User", UserSchema);
