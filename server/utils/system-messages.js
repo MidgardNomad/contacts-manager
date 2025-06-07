49d66870f9c483b97a67b887b6a350bd489d27ad
M-E-Taha <elsha3rawy.btm@gmail.com>
2025-06-08 00:34:19 +0300

    Add auth messages to the system messages.

--- server/utils/system-messages.js
+++ server/utils/system-messages.js
@@ -3,6 +3,11 @@ function getMessage(code) {
 }
 
 //Code and message
-const messages = {};
+const messages = {
+  //AUTH MESSAGES
+  LOGIN_SUCSESS: "User has been logged in successfully.",
+  NO_USER_FOUND: "User is not registered",
+  INVALID_CREDENTIALS: "Invalid Credentials",
+};
 
 module.exports.getMessage = getMessage;
