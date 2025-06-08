179759f2df86e6804ca1dfc1e0cd322ad444f847
M-E-Taha <elsha3rawy.btm@gmail.com>
2025-06-08 03:10:55 +0300

    export the loading animation module from the shared module.

--- client/src/app/modules/shared/shared.module.ts
+++ client/src/app/modules/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonLoadingSpinnerComponent } from './components/button-loading-spinner/button-loading-spinner.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ButtonLoadingSpinnerComponent],
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
-  exports: [CommonModule, ReactiveFormsModule, FormsModule],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ButtonLoadingSpinnerComponent,
+  ],
 })
 export class SharedModule {}
