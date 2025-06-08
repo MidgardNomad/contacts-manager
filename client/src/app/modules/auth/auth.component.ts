d118f9a79b90e914a7ccb09ef310aa71c4f51b34
M-E-Taha <elsha3rawy.btm@gmail.com>
2025-06-08 03:20:17 +0300

    Finish login logic

--- client/src/app/modules/auth/auth.component.ts
+++ client/src/app/modules/auth/auth.component.ts
@@ -7,6 +7,10 @@ import {
   ViewChild,
 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
+import { User } from '../../models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -15,30 +19,59 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './auth.component.scss',
 })
 export class AuthComponent implements OnInit, AfterViewInit, OnDestroy {
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
   //Life-Cycle Hooks
   ngOnInit(): void {
     this.initForm();
   }
   ngAfterViewInit(): void {
-    this.passwordInputField.nativeElement.focus();
+    this.usernameInputField.nativeElement.focus();
   }
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+    this.loginAPISub?.unsubscribe();
+  }
 
   //DOM
   @ViewChild('passwordInputField')
   passwordInputField: ElementRef<HTMLInputElement>;
+  @ViewChild('usernameInputField')
+  usernameInputField: ElementRef<HTMLInputElement>;
+  @ViewChild('togglePasswordButton')
+  togglePasswordButton: ElementRef<HTMLButtonElement>;
   //Private Props
+  //SUBS
+  private loginAPISub: Subscription;
 
   //Private methods
   private initForm() {
     this.signInForm = this.fb.group({
       username: [null, Validators.required],
-      password: [null, [Validators.required, Validators.minLength(8)]],
+      password: [null, [Validators.required, Validators.minLength(5)]],
     });
+  }
+  private loginAction(action: 'enable' | 'disable') {
+    switch (action) {
+      case 'disable':
+        this.signingIn = true;
+        this.signInForm.get('username').disable();
+        this.signInForm.get('password').disable();
+        this.togglePasswordButton.nativeElement.disabled = true;
+        break;
+      case 'enable':
+        this.signingIn = false;
+        this.signInForm.get('username').enable();
+        this.signInForm.get('password').enable();
+        this.togglePasswordButton.nativeElement.disabled = false;
+        break;
+    }
   }
   //Comp Props
   signInForm: FormGroup;
+  signingIn = false;
   //Comp Methods
   togglePassword() {
     this.passwordInputField.nativeElement.type =
@@ -47,7 +80,26 @@ export class AuthComponent implements OnInit, AfterViewInit, OnDestroy {
         : 'password';
   }
   signin() {
-    this.signInForm.get('password').setErrors({ invalid: true });
-    console.log(this.signInForm.value);
+    this.loginAction('disable');
+    const { username, password } = this.signInForm.value;
+    const correctCred =
+      (username === 'user1' && password === 'user1') ||
+      (username === 'user2' && password === 'user2');
+    if (correctCred) {
+      this.loginAPISub = this.authService
+        .login({ username, password })
+        .subscribe({
+          next: (res) => {
+            localStorage.setItem('token', res.data.token);
+            this.router.navigate(['/']);
+          },
+          error: (error) => {
+            console.log(error);
+            this.loginAction('enable');
+          },
+        });
+    } else {
+      console.log(`Throw errors`);
+    }
   }
 }
